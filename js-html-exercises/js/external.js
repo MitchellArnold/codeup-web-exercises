2d9d46bdd911a351a93352821dee3f435585bd1d
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-08-06 09:57:38 -0500

    add js-html-exercises

--- js-html-exercises/js/external.js
+++ js-html-exercises/js/external.js
@@ -1,6 +1,7 @@
-console.log("Hello from external JavaScript.");
-
-alert('Welcome to my Website!');
+"use strict"
+//console.log("Hello from external JavaScript.");
+//
+//alert('Welcome to my Website!');
 //
 //var favoriteColor = prompt('What is your favorite color?');
 //
@@ -14,11 +15,13 @@ alert('Welcome to my Website!');
 //var totalRentalCost = (HerculesRental_per_day + brotherBearRental_per_day + littleMermaidRental_per_day) * dailyRentalRate;
 //var costUnit = "$";
 //alert("Your total cost is:" + costUnit + totalRentalCost);
-
 //
+//// Using the number() method in you prompts eliminates usage of loop checks.
 //
-//var googleHourlyPay = prompt("How much does Google pay per hour?");
-//var amazonHourlyPay = prompt("How much does Amazon pay per hour?");
+////
+//
+//var googleHourlyPay = Number(prompt("How much does Google pay per hour?"));
+//var amazonHourlyPay = Number(prompt("How much does Amazon pay per hour?"));
 //var facebookHourlyPay = prompt("How much does Facebook pay per hour?");
 //
 //var googleHoursWorked = prompt("How many hours worked?");
@@ -26,26 +29,29 @@ alert('Welcome to my Website!');
 //var facebookHoursWorked = prompt("How many hours worked?");
 //var costUnit = "$";
 //var totalPayment = costUnit + ( (googleHourlyPay * googleHoursWorked ) + (amazonHourlyPay * amazonHoursWorked ) + (facebookHourlyPay * facebookHoursWorked ) );
+//// totalPayment += googleHourlyPay * googleHoursWorked;
+//// totalPayment += amazonHourlyPay * amazonHoursWorked;
+//
 //
 //alert("You made" + totalPayment);
 ////
-//
 
-//var classSched = confirm("If the class the class is not full, press Ok.");
-//var conflictSched = confirm("If schedule does not conflict, press Ok.");
+//
+//var classSched = confirm("If the class the class is not full, press OK.");
+//var conflictSched = confirm("If schedule does not conflict, press OK.");
 //var studentEnroll = ( classSched && conflictSched );
 ////== true;
 //alert("It is" + " " + studentEnroll + " " + "that the student may enroll.");
 
 
-var numberOfItems = prompt("How many items?");
-var premiumOffer = confirm("Click ok if you are a premium member.");
-var validOffer = confirm("Click ok if offer is valid.");
-var productDiscountApplied = (validOffer || numberOfItems < 2) && premiumOffer;
-
-
-alert("It is" + " " + productDiscountApplied + " " + "that the offer can be applied.");
+//var numberOfItems = prompt("How many items?");
+//var premiumOffer = confirm("Click ok if you are a premium member.");
+//var validOffer = confirm("Click ok if offer is valid.");
+//var productDiscountApplied = (validOffer || numberOfItems < 2) && premiumOffer;
 //
+//
+//alert("It is" + " " + productDiscountApplied + " " + "that the offer can be applied.");
+////
 //
 //
 
