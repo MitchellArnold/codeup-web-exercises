9d4929a6fda817ba7dd83cbaecf627266748fd46
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-08-07 15:44:00 -0500

    add conditionals-lec

---
+++ js/conditionals-lec.js
@@ -0,0 +1,163 @@
+"use strict";
+
+// var isRaining = true;
+// if(isRaining){
+//     alert("It's raining!");
+// }
+//
+// var isDead = 0;
+// var isAlive;
+// if(isAlive === isDead){
+//     alert("You died!");
+// }
+//
+
+//
+// var weShouldDeleteStuff = confirm('Are you sure you want to delete everything?');
+//
+// console.log("The confirm value is" + weShouldDeleteStuff);
+//
+// if (weShouldDeleteStuff) {
+//     alert("We deleted everything");
+// } else {
+//     alert("Operation Canceled!");
+// }
+
+
+// Drum machine
+//
+// var drum_machine_on = confirm("Is drum machine on?");
+//
+// if(drum_machine_on){
+//     alert("The drums channel is armed and ready.");
+// }else {
+//     alert("Your drum machine is inactive.");
+// }
+
+//
+//
+// var pizzaPreference = prompt("What kind of pizza do you like?");
+//
+// if (pizzaPreference === "pineapple and hot sauce") {
+//     alert("What a coincidence, that's my favorite!");
+// } else if (pizzaPreference === "cheese") {
+//     alert("Just plain cheese? Okay...");
+// } else if (pizzaPreference === "ham");{
+//       alert("Ham is easy and great!");
+// } else {
+//     alert(pizzaPreference + " isn't my favorite, but let's order some!");
+// }
+// }
+//
+
+// var pizzaPreference = prompt("What kind of pizza do you like?");
+//
+// switch(pizzaPreference) {
+//     case "pineapple and hot sauce":
+//         alert("What a coincidence, that's my favorite!");
+//         break;
+//     case "cheese":
+//         alert("Just plain cheese? Okay...");
+//         break;
+//     default:
+//         alert(pizzaPreference + " isn't my favorite, but let's order some!");
+//         break;
+// }
+//// reiterate  <<<
+
+//
+// function checkIfGameIsOver(numberOfLives) {
+//     if(numberOfLives === 0){
+//         // game over
+//         return "Sorry, game is over.";
+//     } else {
+//         return "Play on!";
+//     }
+// }
+//
+// console.log(checkIfGameIsOver(1));
+
+
+//
+// function traffic_light(light_color){
+//    var green = "green";
+//    var yellow = "yellow";
+//    var red = "red";
+//
+//    if (light_color === "green") {
+//        return alert("You are approaching a green light");
+//    } else if (light_color === "yellow") {
+//        return alert("You are approaching a yellow light");
+// } else {
+//         return alert("You are approaching a red light");
+//    }}
+//
+//    console.log(traffic_light("yellow"));
+// //
+// var light_color = prompt("Which light color?");
+//
+// switch(light_color){
+//     case "green":
+//         alert("You are approaching a green light");
+//         break;
+//     case "yellow":
+//         alert("You are approaching a yellow light");
+//         break;
+//     case "red":
+//         alert("You are approaching a red light");
+//         break;
+//     default:
+//         alert("Proceed with caution.");
+// }
+
+
+
+//
+//
+// var message;
+//
+// if (success) {
+//     message = "Operation was successful.";
+// } else {
+//     message = "Oops, something went wrong.";
+// }
+//
+// // the above if/else can be re-written as:
+// var message = (success) ? "Operation was successful." : "Oops, something went wrong.";
+//
+// //
+//
+
+// User checks if shopping is done. If shopping is done, check box and child will receive $5 extra. By default spoiled child will receive $5.
+// var isShoppingDone = false;
+// if(isShoppingDone === true){
+//     return "Child has earned $10 this week.";
+// }
+
+
+// Setup weather dropdown. It's either sunny, rainy, snowy or overcast. If user selects dropdown item it will alert user of selected item.
+// if(weatherOutside === "Sunny"){
+//     return "Sunny";
+// } else if(weatherOutside === "Rainy"){
+//     return "Rainy";
+// } else if(weatherOutside === "Snowy"){
+//     return "Snowy";
+// }else if(weatherOutside === "overcast"){
+//     return "Overcast";
+// }else(weatherOutside === " "){
+//     return "Unknown. Still be careful."
+// }
+//
+// User selects theme via bg-color dropdown. The list item will target the bg property selector and change color according to item selected.
+// var message;
+//
+// if(success) {
+//    message = "white";
+// } else {
+//     message = "black";
+// }
+//
+
+// var message = (success) ? "white" : "black";
+
+
---
+++ js/function_drills.js
@@ -0,0 +1,8 @@
+// Function Drills
+//
+// Functions using only conditionals
+//
+function isOdd(num) {
+    num % 2 == 1;
+    return num;
+}
