c46aa72ad1e75119c3d0bac1dde4d1abe52b1568
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-08-12 13:57:09 -0500

    add loopsexercise

---
+++ js/while.js
@@ -0,0 +1,14 @@
+"use strict";
+// take current var and mult by 2
+// first question
+// the html and js files are created and pushed on separate branch
+
+// second question
+var temp = 2;
+var i = 0; // needs to reach 16
+while( temp <= 65536 ) {
+    console.log(temp)
+    temp *= 2;
+}
+
+// third question
