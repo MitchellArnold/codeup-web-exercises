8674514a4fe85e166ab2192bf51bead469a6b4c7
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-08-13 09:31:56 -0500

    add loops exercise

---
+++ js/for_loops.js
@@ -0,0 +1,41 @@
+"use strict";
+
+//// first question
+// created for_loops.js
+
+//// second question
+//
+// function showMultiplicationTable(multiple) {
+//     for (var i = 0; i <=10; i++){
+//         console.log(multiple + " x " + i + " = " + multiple * i);
+//     }
+//
+// }
+//
+// showMultiplicationTable(7);  // 7 is test case
+
+
+//// third question
+
+// function tenGen(rand){
+//     for (var i = 0; i <= 10; i++){
+//         rand = Math.floor(Math.random() * 180) + 20;
+//         //console.log("Current random #: " + rand);
+//         if (rand % 2 === 0){
+//             console.log(rand + " is even.");
+//         }else {
+//             console.log(rand + " is odd.");
+//         }
+//     }
+// }
+//
+// tenGen();
+
+//// fourth question
+//
+//     var i = 100;
+//     console.log("This is current number: " + i);
+//     for ( i > 0; i -= 5;) {
+//         console.log("This is current number: " + i);
+//     }
+
--- js/while.js
+++ js/while.js
@@ -1,15 +1,70 @@
 "use strict";
 // take current var and mult by 2
-// first question
+//// first question
 // the html and js files are created and pushed on separate branch
 
-// second question
-var temp = 2;
-var i = 0; // needs to reach 16
-while( temp <= 65536 ) {
-    console.log(temp)
-    temp *= 2;
-}
+//// second question
+// var temp = 2;
+// var i = 0; // needs to reach 16
+// while (temp <= 65536) {
+//     console.log(temp)
+//     temp *= 2;
+// }
 
-// third question
-testing
+//// third question
+// var allcones = Math.floor(Math.random() * 50) + 50; // number of total cones
+// var temp_total = 0;  // current total sold "during shift"
+// do {
+//     var temp = Math.floor(Math.random() * 5) + 1; // number of sold cones
+//     console.log("All Cones: " + allcones)
+//     console.log("The first temp: " + temp);
+//     temp_total += temp;
+//     console.log("Temp total: " + temp_total)
+//     console.log("The second temp: " + temp);
+//     if (temp === 1) {
+//         console.log("On this day, at this hour, one cone was sold.")
+//     } else if (temp === 2) {
+//         console.log("Two cones were sold!")
+//     } else if (temp === 3) {
+//         console.log("Three cones were sold.")
+//     } else if (temp === 4) {
+//         console.log("Four cones were sold.")
+//     } else if (temp === 5) {
+//         /// exact amount of cones sold
+//         console.log("5 cones sold.")
+//     } else if (temp > 5) {
+//         /// cannot sell amount over what you have
+//         console.log("Cannot sell more cones than you have silly.")
+//     } else {
+//         /// all the cones are sold at this point
+//         console.log("Good job! You sold all of the cones! GO HOME!!")
+//     }
+// } while (temp_total <= allcones)
+//
+// var allcones = Math.floor(Math.random() * 50) + 50; // number of total cones
+// console.log("All cones: " + allcones)
+// var conesCapPerSale = 5; // max cones amount per transaction
+// var temp_total = 0;
+// do {
+//     var conesPerCustomer = Math.floor(Math.random() * 5) + 1; // number of sold cones
+//     console.log("Cones per customer: " + conesPerCustomer)
+//     temp_total += conesPerCustomer;
+//     console.log("Currently sold : " + temp_total + " cones.")
+//     if (conesPerCustomer <= conesCapPerSale) {
+//         console.log("You have sold " + conesPerCustomer + " cones. Great job!")
+//     } else {
+//         break;
+//         /// needs to allow greater than 5
+//     }
+//
+//
+// } while (temp_total <= allcones)
+
+
+//// refactor to be less static and more dynamic
+
+
+
+
+
+
