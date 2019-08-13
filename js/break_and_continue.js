bea44a8dfe829a5ffae008663da3e06724d3f6a3
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-08-13 13:57:15 -0500

    add loops modify

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,16 +3,20 @@
 
 //// second question
 
-// do {
-//     let oddNumber = +prompt("What is a number between 1 and 50.");
-//     console.log("This is your current odd number: " + oddNumber);
-// } while (oddNumber <= 50);
-//
+do {
+    var oddNumber = +prompt("What is a number between 1 and 50.");
+    if(oddNumber % 2 === 1) {
+        console.log("This is your current odd number: " + oddNumber);
+    }else {
+        console.log("This isn't an odd number.");
+    }
+} while (oddNumber <= 50);
+
 
 
 //// third question
-
-// for (var i = 0; i < 50; i++) {
+//
+// for (var i = 1; i <= 50; i++) {
 //     if (i === 27) {
 //         console.log("Yikes! Skipping number: " + i);
 //         continue;
--- js/for_loops.js
+++ js/for_loops.js
@@ -6,7 +6,7 @@
 //// second question
 //
 // function showMultiplicationTable(multiple) {
-//     for (var i = 0; i <=10; i++){
+//     for (var i = 1; i <=10; i++){
 //         console.log(multiple + " x " + i + " = " + multiple * i);
 //     }
 //
@@ -18,8 +18,8 @@
 //// third question
 
 // function tenGen(rand){
-//     for (var i = 0; i <= 10; i++){
-//         rand = Math.floor(Math.random() * 180) + 20;
+//     for (var i = 1; i <= 10; i++){
+//         rand = Math.floor(Math.random() * 181) + 20;
 //         //console.log("Current random #: " + rand);
 //         if (rand % 2 === 0){
 //             console.log(rand + " is even.");
@@ -32,10 +32,37 @@
 // tenGen();
 
 //// fourth question
+
+// var i = 1;
+// for (var j = 1; j <=9; j++){
+//     console.log(j * i);
+//     i += "1";
+// }
+//
+
+//// another option
+// for (var i =1; i <= 9; i++){
+//     var num = " ";
+//     for (var j = 0; j < i; j++){
+//         num += i
+//     }
+//     console.log(num)
+// }
+
+//// yet another option
+// for (var i=1; i <= 9; i++){
+//     console.log(String(i).repeat(i))
+// }
+
+
+
+//// fifth question
 //
 //     var i = 100;
 //     console.log("This is current number: " + i);
 //     for ( i > 0; i -= 5;) {
 //         console.log("This is current number: " + i);
 //     }
+//
+
 
