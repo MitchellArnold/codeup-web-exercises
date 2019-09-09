6e1f3f424193da3dad3e25dbc97d646badca0083
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-09-09 13:40:56 -0500

    add changes

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -41,5 +41,47 @@ const users = [
     }
 ];
 
-// let language = users.language;
-const languages = users.filter(function (n => );
+
+/// Filter
+const languages = users.filter(function (n) {
+    return n.languages.length >= 3;
+});
+console.log(languages);
+
+// let language = users[n].language    </// unnecessary when you iterate through with n. With arrays, think taking out from array, checking it, and putting it back if it passes ( .filter ), like a bookshelf.
+
+// console.log(users[0].languages);
+
+
+
+/// Map
+const email = users.map(function (n) {
+   return n.email;
+});
+
+console.log(email);
+
+
+
+/// Reduce
+let userCount = users.length;
+console.log(userCount);
+
+const experience = users.reduce((total, person) => {
+    let totalXP = total + person.yearsOfExperience;
+        return totalXP;
+}, 0) / userCount;
+
+console.log(experience);
+
+
+
+const longestEmail = users.reduce((total, account) => {
+    let longest;
+    if(longest > account) {
+        longest = account.email.length;
+        return longest;
+    }
+}, 0);
+
+console.log(longestEmail);
