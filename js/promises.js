fe1ec03d7989a205dec7d2c8e048df350f69c6cc
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-09-10 14:26:05 -0500

    add promises.js

--- js/promises.js
+++ js/promises.js
@@ -32,7 +32,7 @@
 
 ///////////////// Token Exercise ///////////////
 
-let grab = fetch('https://api.github.com/users', {headers: {'Authorization': 'baac39ff807f94f9e39561059f882787a70d452f'}}).then(function (resolve) {
+let grab = fetch('https://api.github.com/users', {headers: {'Authorization': 'token baac39ff807f94f9e39561059f882787a70d452f'}}).then(function (resolve) {
     return resolve.json();
 }).then(function (data) {
     console.log(data);
@@ -56,22 +56,24 @@ let grab = fetch('https://api.github.com/users', {headers: {'Authorization': 'ba
 //
 
 
+// condition check for push date on repo
 
-function getGithubUsernames(user) {
+
+function getGithubUsernames(username) {
     return new Promise(((resolve, reject) => {
-        if(user) {
-            return resolve(
+        if(username) {
+            resolve(grab
 
-                $.get('/users/:username/events')
 
+               'Your username: ' + username
             )
         }else {
-            reject('Please enter a name')
+            reject('Please enter a username')
         }
     }));
 };
 
-console.log(getGithubUsernames('mitchell.arnold'));
+console.log(getGithubUsernames('mitchell'));
 
 
 // later on...
